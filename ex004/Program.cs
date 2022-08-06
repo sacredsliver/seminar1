fee6643809a88c44171b7608d85b14be91e6bf58
SacredSliver <sliver_@mail.ru>
2022-08-06 14:57:54 +0300

    all v2

---
+++ ex004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+int num2 = -num1;
+int count = num2;
+int num3 = num2;
+
+while (count < (num1+1))
+{
+Console.WriteLine(num3);
+num3++;
+count++;
+}
+
---
+++ ex005/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("enter a number:");
+int number = int.Parse(Console.ReadLine());
+int ThirdDigit = number % 10;
+Console.WriteLine(ThirdDigit);
+
